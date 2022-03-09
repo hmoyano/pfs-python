9ceee1c3d6521ba2329f0dc06cc78b8f5d78c0a4
hmoyano <h.moyanove@gmail.com>
2022-03-09 13:28:28 +0100

    add main file

--- main.py
+++ main.py
@@ -2,19 +2,5 @@
 # Version: 0.0.1
 # data: 02/03/2022
 
-# definicion de variables
-
-a = 98
-c = 78
-
-PI = 3.1416
-
-def suma():
-    global PI
-
 if __name__ == '__main__':
-    print(PI)
-    PI = 7
-    print(PI)
-    if a>c:
-        print('A')
+    print("hola mundo")
