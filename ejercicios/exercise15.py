1f2e3de33f5b17a0d5e6db9f03ff4ce518bba465
hmoyano <h.moyanove@gmail.com>
2022-03-14 18:11:01 +0100

    15 16 y 17

---
+++ ejercicios/exercise15.py
@@ -0,0 +1,20 @@
+# Auth: Hector Moyano
+# Exercise: 15
+# data: 14/03/2022
+
+nventas = int(input("Introduce el número total de ventas: "))
+sumventas = 0
+a = 1
+
+while a <= nventas:
+    venta = float(input("Venta nº" + str(a) + ": "))
+    sumventas = float(sumventas) + float(venta)
+    a += 1
+
+if nventas == 1:
+    print("El importe total de la venta es: ", sumventas)
+elif nventas == 0:
+    print("El número total de ventas es 0 y por lo tanto su importe también es 0.")
+else:
+    print("El importe total de las", nventas, "ventas es: ", sumventas)
+
---
+++ ejercicios/exercise16.py
@@ -0,0 +1,14 @@
+# Auth: Hector Moyano
+# Exercise: 16
+# data: 14/03/2022
+
+day = str(input('Introduce un día de la semana para comprobar si es laborable: '))
+
+if  day.upper() == 'SÁBADO' or day.upper() == 'DOMINGO':
+    print('El', day,'es festivo.')
+elif    day.upper() == 'LUNES' or day.upper() == 'MARTES' or day.upper() == 'MIERCOLES':
+    print('El', day, 'es laborable.')
+elif    day.upper() == 'JUEVES' or day.upper() == 'VIERNES':
+    print('El', day, 'es laborable.')
+else:
+    print("No has introducido un día de la semana válido, revisa la ortografía")
